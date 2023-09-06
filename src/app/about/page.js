844b84667c3cc259449fc7802373656bab8f3a4d
Shepon Biswas <sheponbiswas01@gmail.com>
2023-09-07 03:23:58 +0600

    Error fix on about us page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -17,14 +17,14 @@ const AboutPage = () => {
             exceptional gym equipment at affordable prices. But, we dreamed
             bigger than just being an ordinary fitness equipment supplier; we
             aspired to lead the industry. Two decades later, we are proud to
-            have transformed Gym and Fitness into one of Australia's premier
-            online retailers of fitness equipment. We've moved beyond just
-            supplying gear – we've become advocates for fitness, wellbeing and
+            have transformed Gym and Fitness into one of Australias premier
+            online retailers of fitness equipment. We have moved beyond just
+            supplying gear we have become advocates for fitness, wellbeing and
             life enrichment. Our commitment to excellence is reflected not only
-            in the quality equipment we provide but also in the lives we've
-            improved. Our real success lies in the thousands of customers we've
-            empowered to live longer, more joyful, and healthier lives. We're
-            more than just a fitness company – we're your partners in health,
+            in the quality equipment we provide but also in the lives we have
+            improved. Our real success lies in the thousands of customers we have
+            empowered to live longer, more joyful, and healthier lives. We are
+            more than just a fitness company we are your partners in health,
             your cheerleaders on the sideline, motivating you to reach your
             personal fitness goals. Embrace the fitness journey with us and see
             how it transforms your world. Join us in our mission to improve
