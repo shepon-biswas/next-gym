6ad2b74e0b70ff0a98a955bd350287d28b691df3
Shepon Biswas <sheponbiswas01@gmail.com>
2023-09-10 20:11:40 +0600

    fixing error on about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
       <div>
         <PageBanner title={"About Us"}></PageBanner>
       </div>
-      <div className="w-10/12 mx-auto my-14 ">
+      <div className="w-10/12 mx-auto my-14">
         <div>
           <h2 className="text-2xl font-bold text-center">About Us</h2>
         </div>
